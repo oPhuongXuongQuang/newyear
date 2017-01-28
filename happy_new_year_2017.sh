1a23f4f32cc4fbdc48bc57fdb7f73d6bc1287c9b
quangpx <quangpx1@fsoft.com.vn>
2017-01-28 21:05:12 +0700

    happy new year

---
+++ happy_new_year_2017.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+printf "Kính " && sleep 0.5 && printf "Chúc " && sleep 0.5 && printf "Mọi " && sleep 0.5 && echo "Người " && sleep 0.5
+printf "Năm " && sleep 0.5 && printf "Mới " && sleep 0.5 && printf "Đinh " && sleep 0.5 && echo "Dậu " && sleep 0.5
+printf "An " && sleep 0.5 && printf "Khang " && sleep 0.5 && printf "Thịnh " && sleep 0.5 && echo "Vượng " && sleep 0.5
+printf "Vạn " && sleep 0.5 && printf "Sự " && sleep 0.5 && printf "Như " && sleep 0.5 && echo "Ý " && sleep 0.5
+printf "Phát " && sleep 0.5 && printf "Tài " && sleep 0.5 && printf "Phát " && sleep 0.5 && echo "Lộc " && sleep 0.5
+echo "." && sleep 1
+echo "." && sleep 1
+echo "." && sleep 1
+echo "\n\n HAPPY NEW YEAR \n\n"
